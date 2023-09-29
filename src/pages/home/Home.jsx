14d76c44bdacac622878a4a6815f402bea23d838
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-29 18:13:21 +0300

    Adding More Detaim for Home Css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./home.scss";
 import SideBar from "../../components/sidebar/SideBar";
 import Navbar from "../../components/navbar/Navbar";
-
+import Widget from "../../components/widget/Widget";
 
 function Home() {
   return (
@@ -10,7 +10,12 @@ function Home() {
       <SideBar />
       <div className="homeContainer">
         <Navbar />
-        container
+        <div className="widgets">
+          <Widget />
+          <Widget />
+          <Widget />
+          <Widget />
+        </div>
       </div>
     </div>
   );
