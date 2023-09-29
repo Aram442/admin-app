ca66180d145f921d5842d5800314466ec724d942
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-29 17:44:30 +0300

    Adding Navbar Functional and Css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./home.scss";
 import SideBar from "../../components/sidebar/SideBar";
+import Navbar from "../../components/navbar/Navbar";
 
 function Home() {
   return (
     <div className="home">
       <SideBar />
-      <div className="homeContainer"> container</div>
+      <div className="homeContainer">
+        <Navbar />
+        container
+      </div>
     </div>
   );
 }
