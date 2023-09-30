da0201f06c86ac446d9ae70e239585d17f78c16f
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-30 15:04:37 +0300

    adding Two new component to Home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import "./home.scss";
 import SideBar from "../../components/sidebar/SideBar";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
+import Chart from "../../components/chart/Chart";
+import Featured from "../../components/featured/Featured";
 
 function Home() {
   return (
@@ -15,6 +17,10 @@ function Home() {
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
         </div>
       </div>
     </div>
