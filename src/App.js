957e0d95aad6582afbefdfef23ec74f15ed85b51
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-09-30 21:40:27 +0300

    Adding featching to APP.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
+
 function App() {
+  const { darkMode } = useContext(DarkModeContext);
+
   return (
-    <div className="app dark">
+    <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
-            <Route path="login" index element={<Login />} />
+            <Route path="login" element={<Login />} />
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
@@ -23,7 +27,6 @@ function App() {
                 element={<New inputs={userInputs} title="Add New User" />}
               />
             </Route>
-
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
