fb360c80555122ee714c1c981f9d065fafa36257
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-16 18:37:11 +0300

    Adding Sign up function for Login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,38 @@
 import React, { useState } from "react";
 import "./login.scss";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+
 function Login() {
   const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //----------------- SIGN UP WITH EMAIL & PASSWORD --------------//
+  const handleLogin = (e) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
   return (
     <div className="login">
       <form onSubmit={handleLogin}>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Submit</button>
         {error && <span>Wrong Email or Password</span>}
         {/* if Have Error Show this Span */}
