e69e93b57764442e9468f24a485d74785a49c527
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-16 17:44:51 +0300

    adding Code for Login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./login.scss";
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Submit</button>
+        <span>Wrong Email or Password</span>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
