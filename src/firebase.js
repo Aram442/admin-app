d5c7895f293508fdbf7d33c79a217786efd85b9f
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-16 19:11:01 +0300

    Recreating .env  & fixing Signup problem

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBOfvPpb2pmjZiRjFUalBnQgzbe2cOfvSs",
-  authDomain: "react-dashboard-148a0.firebaseapp.com",
-  projectId: "react-dashboard-148a0",
-  storageBucket: "react-dashboard-148a0.appspot.com",
-  messagingSenderId: "134513796307",
-  appId: "1:134513796307:web:6282e70db090fbbbf56026",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
