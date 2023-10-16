8b5747a39fd96f47662a0b257072160bf7f9ac8d
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-16 19:16:01 +0300

    Using navigate

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import "./login.scss";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate(); // I used useNavigate to navigate to othe View.
 
   //----------------- SIGN UP WITH EMAIL & PASSWORD --------------//
   const handleLogin = (e) => {
@@ -16,7 +19,8 @@ function Login() {
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
-        console.log(user);
+        // console.log(user);
+        navigate("/"); // if the Login Successfull go to Home page
       })
       .catch((error) => {
         setError(true);
