4b87e918c72e8ec57cd5e9bc92fbb28ca4cfdfff
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-16 17:53:57 +0300

    adding Auth import for firebase.js

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,11 +4,11 @@ function Login() {
   const [error, setError] = useState(false);
   return (
     <div className="login">
-      <form>
+      <form onSubmit={handleLogin}>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
         <button type="submit">Submit</button>
-        {error && <span>Wrong Email or Password</span>}{" "}
+        {error && <span>Wrong Email or Password</span>}
         {/* if Have Error Show this Span */}
       </form>
     </div>
