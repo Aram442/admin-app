de510f3b8518f5eb2a3fcf56708c3d5e88b4fd2d
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-16 17:23:12 +0300

    Creating firebase file

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOfvPpb2pmjZiRjFUalBnQgzbe2cOfvSs",
+  authDomain: "react-dashboard-148a0.firebaseapp.com",
+  projectId: "react-dashboard-148a0",
+  storageBucket: "react-dashboard-148a0.appspot.com",
+  messagingSenderId: "134513796307",
+  appId: "1:134513796307:web:6282e70db090fbbbf56026",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
