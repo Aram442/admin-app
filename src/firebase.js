f1010374ef7095f6bd3e9c989b652bd9ad3d03eb
Aram A. Ahmad <aramaahmad42@gmail.com>
2023-10-17 09:40:32 +0300

    Adding Firestore config

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-// Your web app's Firebase configuration
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -15,4 +13,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = getAuth();
